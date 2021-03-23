06e1f0ee4a1e4e216c69585d214f750e884c942f
Howard Huang <h.huang65@gmail.com>
2021-03-23 09:16:09 -0700

    Only run install script if in Linux

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #/bin/sh
 
-mkdir -p "${HOME}/.config/waybar"
+if [ $(uname) = "Linux" ]; then
+  mkdir -p "${HOME}/.config/waybar"
 
-ln -sf "${PWD}/config" "${HOME}/.config/waybar/"
-ln -sf "${PWD}/style.css" "${HOME}/.config/waybar/"
-ln -sf "${PWD}/scripts" "${HOME}/.config/waybar/"
+  ln -sf "${PWD}/config" "${HOME}/.config/waybar/"
+  ln -sf "${PWD}/style.css" "${HOME}/.config/waybar/"
+  ln -sf "${PWD}/scripts" "${HOME}/.config/waybar/"
+fi
